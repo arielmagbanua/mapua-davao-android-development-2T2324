fed9f3424641dd1bbd2adb1f8aba41032c3991b4
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-03-08 23:41:23 +0800

    cleaned up

--- ClickRacer/app/src/main/java/com/example/clickracer/MainActivity.kt
+++ ClickRacer/app/src/main/java/com/example/clickracer/MainActivity.kt
@@ -2,16 +2,12 @@ package com.example.clickracer
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -24,7 +20,6 @@ import com.example.clickracer.auth.ui.LoginScreen
 import com.example.clickracer.auth.ui.RegistrationScreen
 import com.example.clickracer.ui.theme.ClickRacerTheme
 import com.example.clickracer.utils.showToast
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
